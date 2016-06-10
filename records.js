2248328fb4a6789402f72f34655eb934d233f19b
cronecode <cronecode@gmail.com>
2016-06-09 23:42:33 -0400

    create records.js with mock data

---
+++ records.js
@@ -0,0 +1,12 @@
+//mock player data
+//possible qualities are: red, yellow, blue, green, circle, square, triangle
+//qualities are not separated by type
+
+var playerData = [
+  {quality: 'red', value: 10},
+  {quality: 'blue', value: 2},
+  {quality: 'circle', value: 3},
+  {quality: 'square', value: 8}
+  ]
+  
+module.exports = playerData
