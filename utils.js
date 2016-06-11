e106c0cffa235059d38e82ee7866e515feacc55c
Liam Atticus Clarke <liam.att.clarke@gmail.com>
2016-06-10 23:37:04 -0400

    Added utils, changed matchmaking implementation

---
+++ utils.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// returns true if array contains value
+exports.arrContains = function (arr, val) {
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) return true
+    }
+    return false
+}
