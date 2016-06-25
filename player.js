a9300d1ef62f87789d11376fcc3f1534ff214958
cronecode <cronecode@gmail.com>
2016-06-25 09:13:20 -0400

    merged record function into player module
    record will never be used independently of player, so it should be a method of player

--- player.js
+++ player.js
@@ -1,9 +1,31 @@
 //pattern: {quality: 'name', value: number}
 //played: 'episodeName'
 
-var player = {
-    pattern: [],
-    played: []
-}
+var pattern = [{quality: 'blue', value: 30}, {quality: 'red', value: 80}]
 
-module.exports = player
+var played = ['first']
+
+exports.pattern = pattern
+
+exports.played = played
+
+exports.record = (quality) => {
+    if (quality.name === ''){
+        return
+    }
+    var exists = pattern.filter((item) => {
+        return item.quality === quality.name
+    })
+    if (exists.length === 0){
+        pattern.push(quality)
+        console.log('QUALITY ACQUIRED: ' + quality.name)
+    } else {
+        pattern.forEach((item) => {
+            if (item.quality === quality.name){
+                quality.value = Number(quality.value)
+                item.value += quality.value
+                console.log('PLAYER IS MORE ' + item.quality + 'THAN EVER')
+            }
+        })
+    }
+}
