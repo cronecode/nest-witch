8d7c745d962b5939d41bbd72e58ecb6112c3cee0
LiamAttClarke <liam.att.clarke@gmail.com>
2016-06-27 18:13:47 -0400

    Revert "sdf"
    This reverts commit 1ac96ad475a530c9e575538f2f38696bdf7ba723, reversing
    changes made to 1ed914240bf9c6339a1f80c4690bec2cf4b0e817.

---
+++ episodeIndex.js
@@ -0,0 +1,13 @@
+//mock episode data
+//possible qualities are: red, yellow, blue, green, circle, square, triangle
+//qualities are not separated by category; any combination is a legal pattern
+
+
+var episodes = [
+  {name: 'ep1', pattern: ['blue', 'square']},
+  {name: 'ep2', pattern: ['red', 'circle']},
+  {name: 'ep3', pattern: ['red', 'green']},
+  {name: 'ep4', pattern: ['green', 'circle']}
+  ]
+  
+module.exports = episodes
