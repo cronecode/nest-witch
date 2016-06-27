61fdb355c874b57fc3faf7b32450bd182f84e277
cronecode <cronecode@gmail.com>
2016-06-27 18:30:25 -0400

    foo

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var pug = require('pug')
-var record = require('./records')
+var episodes = require('./episodeIndex')
+var matchmaker = require('./matchmaker')
+
+var current = matchmaker.current
 
 app.set('port', process.env.PORT || 3000)
 
@@ -13,29 +16,52 @@ app.set('views', './views')
 app.set('view engine', 'pug')
 app.use(express.static(__dirname + '/public'))
 
-var fn = pug.compileFile('./views/index.pug')
-
-app.get('/', function(req, res){
-    var first = require('./first.json')
-    var episode = first
-    var i = req.body.link || 0
-    var scene = episode.scenes[i]
-    console.log(scene)
-    res.render('index', {header: scene.header, text: scene.text, choices: scene.choices})
+app.get('/', function(req, res) {
+  res.render('index')
 })
 
-app.post('/', function(req, res){
-    var first = require('./first.json')
-    var episode = first
-    console.log(req.body)
-    var quality = req.body.quality
-    record(quality)
-    var i = req.body.link
+app.get('/home', function(req, res){
+  res.render('home')
+})
+
+
+app.get('/game', function(req, res){
+  res.render('game');
+    /*console.log(player.pattern)
+    var match = matchmaker.match(player.pattern, episodes)
+    console.log('MATCH = ' + match)
+    var url = './' + match + '.json'
+    episode = require(url)
+    var i = req.body.link || 0
     var scene = episode.scenes[i]
-    console.log(scene)
-    var html = fn({header: scene.header, text: scene.text, choices: scene.choices})
-    res.status(200).send(html)
+    res.render('index', {header: scene.header, text: scene.text, choices: scene.choices})*/
+})
+
+app.post('/game', function(req, res){
+    var pattern = req.body.pattern
+    var current = req.body.episode
+    var i = req.body.index
+    url = './' + current + '.json'
+    var episode = require(url)
+    var scene = episode.scenes[i]
+    if (!scene) {
+      res.status(404).send('Episode index not found')
+    }
+    var bodyPartial = pug.compileFile('./views/game_partial.pug')({header: scene.header, text: scene.text, choices: scene.choices})
+    res.status(200).send(bodyPartial)
     res.end()
+})
+
+//THIS IS WHAT YOU'RE WORKING ON!!!
+app.post('/home', function(req, res){
+    var pattern = req.body.pattern
+    console.log('PATTERN RECEIVED @ HOME/POST: ' + pattern)
+    console.log(current)
+    matchmaker.match(pattern, episodes)
+    var current = matchmaker.episode()
+    console.log(current)
+    res.status(200).send(current)
+    res.end
 })
 
 app.listen(app.get('port'), function() {
